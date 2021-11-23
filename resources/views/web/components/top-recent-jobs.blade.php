e5e3cc801a091cdf88d71b9a7ce68d8812663112
Samun Mollik <samun.aleshasolutions@gmail.com>
2021-11-23 16:29:50 +0600

    Webpage Fronted Changed

--- resources/views/web/components/top-recent-jobs.blade.php
+++ resources/views/web/components/top-recent-jobs.blade.php
@@ -12,98 +12,180 @@
                 <br>
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded" style="margin-left: 12%">
+                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded">
                             <div class="card-body">
-                                <div class="">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="#"><h5 class="card-title card-job-post-title">Service Electric Line Repair with Long Job Title For Testing</h5></a>
+                                        <h6 class="card-subtitle mb-2 mt-2 text-muted">Service Category: Electrician</h6>
+                                        <span class="text-info"><i class="fa fa-user mr-2"></i>Shah Md. Iktakhairul Islam</span><br>
+                                        <span><i class="fa-solid fa-location-dot mr-2"></i>143/E Jamtola Rd, Dhaka, Bangladesh</span>
+                                    </div>
+                                    <div class="col-sm-1 text-right">
+                                        <a class="" data-toggle="collapse" href="#collapseJobInfoOne" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-angle-down fa-lg"></i></a>
+                                    </div>
                                 </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
-                                </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="collapse" id="collapseJobInfoOne">
+                                        <hr>
+                                        <span><i class="fa fa-info mr-2"></i> Budget: 2500 BDT</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> Required Person: 12</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> 17+ People Applied</span>
+                                        
+                                    </div>
                                 </div>
                             </div>
+                            
+                            
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded" style="margin-left: 12%">
+                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded">
                             <div class="card-body">
-                                <div class="">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="#"><h5 class="card-title card-job-post-title">Service Electric Line Repair with Long Job Title For Testing</h5></a>
+                                        <h6 class="card-subtitle mb-2 mt-2 text-muted">Service Category: Electrician</h6>
+                                        <span class="text-info"><i class="fa fa-user mr-2"></i>Shah Md. Iktakhairul Islam</span><br>
+                                        <span><i class="fa-solid fa-location-dot mr-2"></i>143/E Jamtola Rd, Dhaka, Bangladesh</span>
+                                    </div>
+                                    <div class="col-sm-1 text-right">
+                                        <a class="" data-toggle="collapse" href="#collapseJobInfoOne" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-angle-down fa-lg"></i></a>
+                                    </div>
                                 </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
-                                </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="collapse" id="collapseJobInfoOne">
+                                        <hr>
+                                        <span><i class="fa fa-info mr-2"></i> Budget: 2500 BDT</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> Required Person: 12</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> 17+ People Applied</span>
+                                        
+                                    </div>
                                 </div>
                             </div>
+                            
+                            
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded" style="margin-left: 12%">
+                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded">
                             <div class="card-body">
-                                <div class="">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="#"><h5 class="card-title card-job-post-title">Service Electric Line Repair with Long Job Title For Testing</h5></a>
+                                        <h6 class="card-subtitle mb-2 mt-2 text-muted">Service Category: Electrician</h6>
+                                        <span class="text-info"><i class="fa fa-user mr-2"></i>Shah Md. Iktakhairul Islam</span><br>
+                                        <span><i class="fa-solid fa-location-dot mr-2"></i>143/E Jamtola Rd, Dhaka, Bangladesh</span>
+                                    </div>
+                                    <div class="col-sm-1 text-right">
+                                        <a class="" data-toggle="collapse" href="#collapseJobInfoOne" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-angle-down fa-lg"></i></a>
+                                    </div>
                                 </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
-                                </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="collapse" id="collapseJobInfoOne">
+                                        <hr>
+                                        <span><i class="fa fa-info mr-2"></i> Budget: 2500 BDT</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> Required Person: 12</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> 17+ People Applied</span>
+                                        
+                                    </div>
                                 </div>
                             </div>
+                            
+                            
                         </div>
                     </div>
+                    
                 </div>
+
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded" style="margin-left: 12%">
+                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded">
                             <div class="card-body">
-                                <div class="">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="#"><h5 class="card-title card-job-post-title">Service Electric Line Repair with Long Job Title For Testing</h5></a>
+                                        <h6 class="card-subtitle mb-2 mt-2 text-muted">Service Category: Electrician</h6>
+                                        <span class="text-info"><i class="fa fa-user mr-2"></i>Shah Md. Iktakhairul Islam</span><br>
+                                        <span><i class="fa-solid fa-location-dot mr-2"></i>143/E Jamtola Rd, Dhaka, Bangladesh</span>
+                                    </div>
+                                    <div class="col-sm-1 text-right">
+                                        <a class="" data-toggle="collapse" href="#collapseJobInfoOne" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-angle-down fa-lg"></i></a>
+                                    </div>
                                 </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
-                                </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="collapse" id="collapseJobInfoOne">
+                                        <hr>
+                                        <span><i class="fa fa-info mr-2"></i> Budget: 2500 BDT</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> Required Person: 12</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> 17+ People Applied</span>
+                                        
+                                    </div>
                                 </div>
                             </div>
+                            
+                            
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded" style="margin-left: 12%">
+                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded">
                             <div class="card-body">
-                                <div class="">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="#"><h5 class="card-title card-job-post-title">Service Electric Line Repair with Long Job Title For Testing</h5></a>
+                                        <h6 class="card-subtitle mb-2 mt-2 text-muted">Service Category: Electrician</h6>
+                                        <span class="text-info"><i class="fa fa-user mr-2"></i>Shah Md. Iktakhairul Islam</span><br>
+                                        <span><i class="fa-solid fa-location-dot mr-2"></i>143/E Jamtola Rd, Dhaka, Bangladesh</span>
+                                    </div>
+                                    <div class="col-sm-1 text-right">
+                                        <a class="" data-toggle="collapse" href="#collapseJobInfoOne" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-angle-down fa-lg"></i></a>
+                                    </div>
                                 </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
-                                </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="collapse" id="collapseJobInfoOne">
+                                        <hr>
+                                        <span><i class="fa fa-info mr-2"></i> Budget: 2500 BDT</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> Required Person: 12</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> 17+ People Applied</span>
+                                        
+                                    </div>
                                 </div>
                             </div>
+                            
+                            
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded" style="margin-left: 12%">
+                        <div class="card border-0 mb-4 shadow p-3 mb-5 bg-white rounded">
                             <div class="card-body">
-                                <div class="">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="#"><h5 class="card-title card-job-post-title">Service Electric Line Repair with Long Job Title For Testing</h5></a>
+                                        <h6 class="card-subtitle mb-2 mt-2 text-muted">Service Category: Electrician</h6>
+                                        <span class="text-info"><i class="fa fa-user mr-2"></i>Shah Md. Iktakhairul Islam</span><br>
+                                        <span><i class="fa-solid fa-location-dot mr-2"></i>143/E Jamtola Rd, Dhaka, Bangladesh</span>
+                                    </div>
+                                    <div class="col-sm-1 text-right">
+                                        <a class="" data-toggle="collapse" href="#collapseJobInfoOne" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-angle-down fa-lg"></i></a>
+                                    </div>
                                 </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
-                                </div>
-                                <div class="mt-4">
-                                    <a href="#" class="pl-4 pr-4 pt-3 pb-3">3 Openings</a>
+                                <div class="row">
+                                    <div class="collapse" id="collapseJobInfoOne">
+                                        <hr>
+                                        <span><i class="fa fa-info mr-2"></i> Budget: 2500 BDT</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> Required Person: 12</span><br>
+                                        <span><i class="fa fa-info mr-2"></i> 17+ People Applied</span>
+                                        
+                                    </div>
                                 </div>
                             </div>
+                            
+                            
                         </div>
                     </div>
+                    
                 </div>
+                
                 </div>
             </div>
         </div>
