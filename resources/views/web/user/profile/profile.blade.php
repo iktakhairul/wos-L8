707205aba45feb5d3ff950b74f3f35860e1f0a10
Iktakhairul <iktakhairul.aleshasolutions@gmail.com>
2021-12-05 00:43:58 +0600

    profiles basic info update working

--- resources/views/web/user/profile/profile.blade.php
+++ resources/views/web/user/profile/profile.blade.php
@@ -21,9 +21,8 @@
                 <div class="col-12 p-0">
                     <nav>
                         <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Basic Info</a>
+                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Documents</a>
                             <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="false">About</a>
                         </div>
                     </nav>
@@ -37,38 +36,88 @@
                                         <a href="#" class="btn btn-light btn-sm">Edit</a>
                                     </div>
                                 </div>
-                                <div class="row mt-2">
-                                    <div class="col-md-6"><label for="fullName" class="profile-labels">Full Name</label><input name="fullName" id="fullName" type="text" class="form-control" placeholder="Full Name" value="{{ $user->user_profile->full_name ?? '' }}"></div>
-                                    <div class="col-md-6"><label for="name" class="profile-labels">User Name</label><input name="name" id="name" type="text" class="form-control" value="{{ $user->name ?? '' }}" placeholder="User Name"></div>
-                                </div>
-                                <div class="row mt-2">
-                                    <div class="col-md-6"><label for="email" class="profile-labels">Email</label><input name="email" id="email" type="email" class="form-control" placeholder="Email" value="{{ $user->email ?? '' }}"></div>
-                                    <div class="col-md-6"><label for="contactNumber" class="profile-labels">Contact Number</label><input name="contactNumber" id="contactNumber" type="text" class="form-control" value="{{ $user->contact_number ?? '' }}" placeholder="+8801683201359"></div>
-                                </div>
-                                <div class="row mt-2">
-                                    <div class="col-md-6"><label for="fatherName" class="profile-labels">Father Name</label><input name="fatherName" id="fatherName" type="text" class="form-control" placeholder="Father Name" value="{{ $user->user_profile->father_name ?? '' }}"></div>
-                                    <div class="col-md-6"><label for="motherName" class="profile-labels">Mother Name</label><input name="motherName" id="motherName" type="text" class="form-control" value="{{ $user->user_profile->mother_name ?? '' }}" placeholder="Mother Name"></div>
-                                </div>
-                                <div class="row mt-2">
-                                    <div class="col-md-6"><label for="dob" class="profile-labels">Date Of Birth</label><input name="dob" id="dob" type="text" class="form-control" placeholder="2021-01-18" value="{{ $user->user_profile->dob ?? '' }}"></div>
-                                    <div class="col-md-6"><label for="gender" class="profile-labels">Gender</label><input name="gender" id="gender" type="text" class="form-control" value="{{ $user->user_profile->gender ?? '' }}" placeholder="Gender"></div>
-                                </div>
-                                <div class="mt-5 text-center">
-                                    <button class="btn btn-primary" type="button">Save Profile</button>
-                                </div>
+                                <form class="form-horizontal" role="form" method="POST" action="{{ route('profile.profiles.update', $user->id) }}" enctype="multipart/form-data">
+                                    @csrf
+                                    {{method_field('PATCH')}}
+                                    <div class="row mt-2">
+                                        <div class="col-md-6">
+                                            <label for="fullName" class="profile-labels">Full Name<small class="text-danger">*</small></label>
+                                            <input name="fullName" id="fullName" type="text" class="form-control" placeholder="Full Name" value="{{ !empty($user->user_profile->full_name) ? $user->user_profile->full_name : old('fullName') }}">
+                                            @if($errors->has('fullName'))
+                                                <small class="text-danger">{{ $errors->first('fullName') }}</small>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="name" class="profile-labels">Name<small class="text-danger">*</small></label>
+                                            <input name="name" id="name" type="text" class="form-control" value="{{ !empty($user->name) ? $user->name : old('name') }}" placeholder="Name">
+                                            @if($errors->has('name'))
+                                                <small class="text-danger">{{ $errors->first('name') }}</small>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="row mt-2">
+                                        <div class="col-md-6">
+                                            <label for="email" class="profile-labels">Email<small class="text-danger">*</small></label>
+                                            <input name="email" id="email" type="email" class="form-control" placeholder="Email" value="{{ !empty($user->email) ? $user->email : old('email') }}">
+                                            @if($errors->has('email'))
+                                                <small class="text-danger">{{ $errors->first('email') }}</small>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="contactNumber" class="profile-labels">Contact Number<small class="text-danger">*</small></label>
+                                            <input name="contactNumber" id="contactNumber" type="text" class="form-control" value="{{ !empty($user->contact_number) ? $user->contact_number : old('contactNumber') }}" placeholder="+8801683201359">
+                                            @if($errors->has('contactNumber'))
+                                                <small class="text-danger">{{ $errors->first('contactNumber') }}</small>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="row mt-2">
+                                        <div class="col-md-6">
+                                            <label for="fatherName" class="profile-labels">Father Name</label>
+                                            <input name="fatherName" id="fatherName" type="text" class="form-control" placeholder="Father Name" value="{{ !empty($user->user_profile->father_name) ? $user->user_profile->father_name : old('fatherName') }}">
+                                            @if($errors->has('fatherName'))
+                                                <small class="text-danger">{{ $errors->first('fatherName') }}</small>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="motherName" class="profile-labels">Mother Name</label>
+                                            <input name="motherName" id="motherName" type="text" class="form-control" value="{{ !empty($user->user_profile->mother_name) ? $user->user_profile->mother_name : old('motherName') }}" placeholder="Mother Name">
+                                            @if($errors->has('motherName'))
+                                                <small class="text-danger">{{ $errors->first('motherName') }}</small>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="row mt-2">
+                                        <div class="col-md-6">
+                                            <label for="dob" class="profile-labels">Date Of Birth<small class="text-danger">*</small></label>
+                                            <input name="dob" id="dob" type="text" class="form-control" placeholder="2021-01-18" value="{{ !empty($user->user_profile->dob) ? $user->user_profile->dob : old('dob') }}">
+                                            @if($errors->has('dob'))
+                                                <small class="text-danger">{{ $errors->first('dob') }}</small>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="gender" class="profile-labels">Gender<small class="text-danger">*</small></label>
+                                            <select class="form-control" name="gender" id="gender">
+                                                <option value="" @if(empty($user->user_profile->gender)) selected @endif>Select One</option>
+                                                <option value="male" @if($user->user_profile->gender === 'male') selected @endif>Male</option>
+                                                <option value="female" @if($user->user_profile->gender === 'female') selected @endif>Female</option>
+                                                <option value="others" @if($user->user_profile->gender === 'others') selected @endif>Others</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 text-right">
+                                        <button class="btn btn-primary" type="submit">Save To Profile</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                         {{--Tab Start--}}
                         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                            Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
-                        </div>
-                        {{--Tab Start--}}
-                        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                            Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+
                         </div>
                         {{--Tab Start--}}
                         <div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
-                            Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+
                         </div>
                     </div>
 
